1a6f8a48650d3c586b040eb69cc862205655b054
Mickaël Seznec <mickael.seznec@gmail.com>
2019-12-14 23:29:15 +0100

    Update config for president

--- shell/local.sh
+++ shell/local.sh
@@ -1,7 +1,11 @@
-# is_zsh || (which zsh > /dev/null && exec zsh)
 # is_zsh && hash -d l=/home_local/$(whoami)
 
 # export CONDA_PATH="/home/seznec/anaconda3"
 # export PATH="$PATH:/usr/local/cuda/bin:/usr/local/texlive/2018/bin/x86_64-linux"
 
-export CONDA_PATH="$HOME/anaconda3"
+# export CONDA_PATH="$HOME/anaconda3"
+
+# if is_zsh; then :;
+# else
+#     which zsh > /dev/null && exec zsh
+# fi
